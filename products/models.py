8c41c5520d1139b02860fa2f1e8949078c5b0eb1
Owen Cotter <owenjames97@outlook.com>
2025-01-13 16:14:09 +0000

    Added products app, models and fixtures

---
+++ products/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    sku = models.CharField(max_length=20, unique=True)
+    type = models.CharField(max_length=100)
+    abv = models.FloatField(
+        blank=True, null=True, 
+        help_text="Alcohol By Volume in percentage (e.g., 5.2). Leave blank for non-beverage products."
+    )
+    description = models.TextField()
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.CASCADE, 
+        related_name="products"
+    )
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
