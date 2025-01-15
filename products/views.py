4b618c98e8c114ce6fd26e2ea282644b2e46762f
Owen Cotter <owenjames97@outlook.com>
2025-01-15 12:17:37 +0000

    Added categories and corrected images uploading functionality

--- products/views.py
+++ products/views.py
@@ -1,28 +1,45 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 def all_products(request):
-    """ A view to show all products including sorting and search queries """
-# logic taken from boutique ado walkthrough 
+    """ A view to show all products, including sorting and search queries """
+
     products = Product.objects.all()
+    categories = Category.objects.all()  # Ensure categories are included
     query = None
+    current_categories = None
+    sort = None
 
     if request.GET:
+        if 'category' in request.GET:
+            current_categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=current_categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-            
+
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+        if 'sort' in request.GET:
+            sort = request.GET['sort']
+            if sort == 'price':
+                products = products.order_by('price')
+            elif sort == 'rating':
+                products = products.order_by('-rating')
+
     context = {
         'products': products,
+        'categories': categories,  # Pass categories to the template
         'search_term': query,
+        'current_categories': current_categories,
+        'current_sort': sort,
     }
 
     return render(request, 'products/products.html', context)
@@ -38,3 +55,4 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
