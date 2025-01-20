8fa533f8f50a7ac3fb9dd39e7016bebea9670bfc
Owen Cotter <owenjames97@outlook.com>
2025-01-20 14:52:04 +0000

    Added stripe elements - boutique ado

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QVXn0FZfI4sG0CkYDEojavmVZHMeLv9a0AoGLqEMya0o7Dob6DKxQifltbSfFoMspvRxQnMjrg5A7IbkCYP3F420004WNosR7',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
