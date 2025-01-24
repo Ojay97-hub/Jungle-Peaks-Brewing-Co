bb63f9a1953abadc536de6662532472b57103524
Owen Cotter <owenjames97@outlook.com>
2025-01-24 11:48:58 +0000

    Debug to true

--- jungle_peaks_brewing/settings.py
+++ jungle_peaks_brewing/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-ojay97hub-junglepeaksbr-rlpjcwm6lxr.ws.codeinstitute-ide.net',
