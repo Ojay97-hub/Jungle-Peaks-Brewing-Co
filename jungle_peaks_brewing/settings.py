f6aaebea080a8bb08f9653007b9c3ef342bf74fe
Owen Cotter <owenjames97@outlook.com>
2025-01-25 16:01:32 +0000

    started the home template

--- jungle_peaks_brewing/settings.py
+++ jungle_peaks_brewing/settings.py
@@ -200,8 +200,6 @@ USE_AWS = True
 
 
 STATIC_URL = '/static/'
-
-STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
