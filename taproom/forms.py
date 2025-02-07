e41f0b63f260242f3aa10d0022a0956818f03891
Ojay97-hub <owenjames97@outlook.com>
2025-02-07 16:14:55 +0000

    styled the booking forms better

--- taproom/forms.py
+++ taproom/forms.py
@@ -6,6 +6,16 @@ class BookingForm(forms.ModelForm):
         model = Booking
         fields = ['name', 'email', 'phone', 'date', 'time', 'guests', 'special_requests']
         widgets = {
-            'date': forms.DateInput(attrs={'type': 'date'}),
-            'time': forms.TimeInput(attrs={'type': 'time'}),
+            'name': forms.TextInput(attrs={'class': 'custom-form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'custom-form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'custom-form-control'}),
+            'date': forms.DateInput(attrs={'type': 'date', 'class': 'custom-form-control'}),
+            'time': forms.TimeInput(attrs={'type': 'time', 'class': 'custom-form-control'}),
+            'guests': forms.NumberInput(attrs={'class': 'custom-form-control'}),
+            'special_requests': forms.Textarea(attrs={'class': 'custom-form-control', 'rows': 4}),
         }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.setdefault('class', 'custom-form-control')
